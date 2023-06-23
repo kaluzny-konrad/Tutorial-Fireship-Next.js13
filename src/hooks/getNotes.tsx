c134ce3429f8f0b286c4b97f0d8a343d919b571b
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-06-23 16:30:00 +0200

    end project

--- src/hooks/getNotes.tsx
+++ src/hooks/getNotes.tsx
@@ -1,13 +1,17 @@
 import PocketBase from "pocketbase";
 
-export const dynamic = "auto",
-  dynamicParams = true,
-  revalidate = 0,
-  fetchCache = "auto",
-  runtime = "nodejs",
-  preferredRegion = "auto";
+// export const dynamic = 'auto',
+//   dynamicParams = true,
+//   revalidate = 0,
+//   fetchCache = 'auto',
+//   runtime = 'nodejs',
+//   preferredRegion = 'auto'
+
+
 export async function getNotes() {
-  const db = new PocketBase("http://127.0.0.1:8090");
-  const data = await db.collection("notes").getList();
+  // const db = new PocketBase('http://127.0.0.1:8090');
+  // const result = await db.records.getList('notes');
+  const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', { cache: 'no-store' });
+  const data = await res.json();
   return data?.items as any[];
 }
